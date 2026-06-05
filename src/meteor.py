b3af69204bfcb34d549409ec912d67e96ce269e8
Gustavo <galvescsousa@gmail.com>
2026-06-05 20:08:30 -0300

    feat: Adiciona modulo de meteoros (Semana 2)

---
+++ src/meteor.py
@@ -0,0 +1,49 @@
+import random
+
+# cria um novo meteoro
+def criar_meteoro():
+    meteoro = {
+        "x": random.randint(0, 760),
+        "y": -40,
+        "velocidade": 5,
+        "tamanho": 40
+    }
+
+    return meteoro
+
+# move um meteoro
+def mover_meteoro(meteoro):
+    meteoro["y"] += meteoro["velocidade"]
+# move todos meteoros da lista
+def mover_meteoros(meteoros):
+    for meteoro in meteoros:
+        mover_meteoro(meteoro)
+
+# Remove meteoros que saíram da tela
+def remover_meteoros(meteoros, altura_tela):
+    meteoros[:] = [
+        meteoro
+        for meteoro in meteoros
+        if meteoro["y"] < altura_tela
+    ]
+
+# TESTE
+if __name__ == "__main__":
+
+    meteoros = []
+
+    for i in range(5):
+        meteoros.append(criar_meteoro())
+
+    print("Meteoros criados:")
+    print(meteoros)
+
+    mover_meteoros(meteoros)
+
+    print("\nMeteoros após movimento:")
+    print(meteoros)
+
+    remover_meteoros(meteoros, 600)
+
+    print("\nMeteoros após remoção:")
+    print(meteoros)
