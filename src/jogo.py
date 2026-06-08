bf1719bb0e4d8b32265cc56b810f84b37e7cb3de
Gustavo <galvescsousa@gmail.com>
2026-06-08 20:47:24 -0300

    Semana 2 concluída

--- src/jogo.py
+++ src/jogo.py
@@ -10,6 +10,9 @@ from src.interface import (
     desenhar_vidas
 )
 
+from src.jogador import Jogador
+from src.tiro import Tiro
+
 def executar_jogo():
     """Executa o loop principal do jogo e controla estado, colisões e pontuação."""
     pygame.init()
@@ -23,15 +26,32 @@ def executar_jogo():
     meteoros = []
     pontos = 0
     vidas = 3
+    jogador = Jogador()
+    lista_tiros = []
 
     rodando = True
 
     while rodando:
+
         relogio.tick(60)
 
         for evento in pygame.event.get():
+
             if evento.type == pygame.QUIT:
                 rodando = False
+
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_SPACE:
+
+                    novo_tiro = Tiro(
+                        jogador.x + jogador.largura,
+                        jogador.y + jogador.altura // 2
+                    )
+
+                    lista_tiros.append(novo_tiro)
+
+        teclas = pygame.key.get_pressed()
+        jogador.movimentar(teclas)
 
         if len(meteoros) < 5:
             meteoros.append(criar_meteoro())
@@ -39,7 +59,19 @@ def executar_jogo():
         mover_meteoros(meteoros)
         remover_meteoros(meteoros, 600)
 
+        for tiro in lista_tiros[:]:
+
+            tiro.atualizar()
+
+            if tiro.rect.x > 800:
+                lista_tiros.remove(tiro)
+
         tela.fill((0, 0, 0))
+
+        jogador.desenhar(tela)
+
+        for tiro in lista_tiros:
+            tiro.desenhar(tela)
 
         for meteoro in meteoros:
             pygame.draw.circle(
