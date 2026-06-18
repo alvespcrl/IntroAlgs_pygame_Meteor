908df3e1e818c9b16802e89cfd356fedecae6b7f
lourenco <lourencohamdan80@gmail.com>
2026-06-18 17:34:07 -0300

    Adicionado modulo de sons e organizacao de audio

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from src.jogo import executar_jogo
 if __name__ == "__main__":
     # Ponto de entrada da aplicação.
     executar_jogo()
+
+
+from src.sons import GerenciadorAudio
+
+audio = GerenciadorAudio()
+
+audio.iniciar_musica_fundo(volume=0.3) 
